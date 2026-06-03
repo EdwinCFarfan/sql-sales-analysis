06492130a55ea6e59e01c6747ad61c7df6f71201
EdwinFarfan <edwincf24@gmail.com>
2026-06-02 22:36:21 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,26 @@
+-- Top 5 clientes con mayor gasto
+
+SELECT
+    cliente,
+    SUM(total) AS total_gastado
+FROM ventas
+GROUP BY cliente
+ORDER BY total_gastado DESC
+LIMIT 5;
+
+
 -- Productos más vendidos
 
 SELECT
     producto,
-    SUM(cantidad) AS total
+    SUM(cantidad) AS unidades_vendidas
 FROM ventas
 GROUP BY producto
-ORDER BY total DESC
+ORDER BY unidades_vendidas DESC;
+
+
+-- Venta promedio
+
+SELECT
+    AVG(total) AS venta_promedio
+FROM ventas;
